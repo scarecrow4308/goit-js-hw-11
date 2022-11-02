7e7848ecac78804aaf495c0edb77008020ddf94e
scarecrow <trender.man21@gmail.com>
2022-11-02 15:48:33 +0200

    hw11

---
+++ src/js/fetchImages.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export class ImageClass {
+  #API_KEY = '30576183-1ebdc1a7ab65c6ad0d15a4f10';
+  #BASE_URL = 'https://pixabay.com/api/';
+  constructor() {
+    this.query = null;
+    this.page = 1;
+    this.totalHits = null;
+    this.total_pages = 0;
+    this.per_page = 20;
+  }
+  async fetchPhotos() {
+    try {
+      const { data } = await axios.get(this.#BASE_URL, {
+        params: {
+          q: this.query,
+          page: this.page,
+          per_page: this.per_page,
+          orientation: 'vertical',
+          key: this.#API_KEY,
+        },
+      });
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import photoCard from '../templates/photoCard.hbs';
+import { ImageClass } from './fetchImages';
+
+const formEl = document.querySelector('.header__search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+const galleryEl = document.querySelector('.gallery');
+const images = new ImageClass();
+
+const getImages = data => {
+  const { hits, totalHits, total } = data;
+  loadMoreBtn.classList.remove('visually-hidden');
+  images.total_pages = Math.ceil(totalHits / images.per_page);
+
+  if (!total) {
+    loadMoreBtn.classList.add('visually-hidden');
+    Notiflix.Notify.failure(
+      'Oops, there are no images mathis your search query. Please try again.'
+    );
+    return;
+  }
+
+  hits.forEach(el => {
+    galleryEl.insertAdjacentHTML('beforeend', photoCard(el));
+  });
+
+  const lightBoxGallery = new SimpleLightbox('.gallery a');
+  lightBoxGallery.on('show.simplelightbox', function () {});
+};
+
+const removeLoadMoreBtn = () => {
+  if (images.page === images.total_pages) {
+    loadMoreBtn.classList.add('visually-hidden');
+  }
+  return;
+};
+
+loadMoreBtn.addEventListener('click', async () => {
+  images.page++;
+  const data = await images.fetchPhotos();
+
+  getImages(data);
+
+  removeLoadMoreBtn();
+});
+
+const onSubmit = async e => {
+  e.preventDefault();
+
+  const inputEl = formEl.querySelector('.header__input');
+
+  images.query = inputEl.value;
+  images.page = 1;
+
+  const data = await images.fetchPhotos();
+
+  galleryEl.innerHTML = '';
+
+  getImages(data);
+
+  if (data.totalHits > 1) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
+  removeLoadMoreBtn();
+};
+
+formEl.addEventListener('submit', onSubmit);
